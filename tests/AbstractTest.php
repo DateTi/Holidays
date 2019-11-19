b9783e267ca36bb809ca7e17c6675f00f4875fef
Jan Galek <admin@gcore.cz>
2019-11-19 20:09:50 +0100

    Removed tests, some country dont celebrate

--- tests/AbstractTest.php
+++ tests/AbstractTest.php
@@ -21,48 +21,4 @@ abstract class AbstractTest extends TestCase
         $this->easterHoliday = Mockery::mock(EasterHolidayInterface::class);
         parent::setUp();
     }
-
-    /**
-     * @test
-     */
-    public function isHolidayGoodFriday()
-    {
-        $this->easterHoliday->expects('getGoodFriday')->andReturn(new \DateTime('2018-04-02'));
-        $this->easterHoliday->expects('getMonday')->andReturn(new \DateTime('2018-03-30'));
-        $date = new DateTi('2018-03-30', new \DateTimeZone('Europe/Prague'));
-        $this->assertEquals(true, $this->holiday->isHoliday($date));
-    }
-
-    /**
-     * @test
-     */
-    public function isHolidayEasterMonday()
-    {
-        $this->easterHoliday->expects('getGoodFriday')->andReturn(new \DateTime('2018-04-02'));
-        $this->easterHoliday->expects('getMonday')->andReturn(new \DateTime('2018-03-30'));
-        $date = new DateTi('2018-04-02', new \DateTimeZone('Europe/Prague'));
-        $this->assertEquals(true, $this->holiday->isHoliday($date));
-    }
-
-    /**
-     * @test
-     */
-    public function isHolidayNewYear()
-    {
-        $this->easterHoliday->expects('getGoodFriday')->andReturn(new \DateTime('2018-04-02'));
-        $this->easterHoliday->expects('getMonday')->andReturn(new \DateTime('2018-03-30'));
-        $date = new DateTi('2020-01-01', new \DateTimeZone('Europe/Prague'));
-        $this->assertEquals(true, $this->holiday->isHoliday($date));
-    }
-
-    /**
-     * @test
-     */
-    public function isHolidayWorkDay()
-    {
-        $this->easterHoliday->expects('getGoodFriday')->andReturn(new \DateTime('2018-04-02'));
-        $this->easterHoliday->expects('getMonday')->andReturn(new \DateTime('2018-03-30'));
-        $date = new DateTi('2020-01-02', new \DateTimeZone('Europe/Prague'));
-        $this->assertEquals(false, $this->holiday->isHoliday($date));
-    }
 }
